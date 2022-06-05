8ede588e7b104bf2a0538874e46a7fbb2b877ed2
DanielAros <danielaros200@gmail.com>
2022-06-04 22:25:01 -0500

    A funcionality has been added to add a word by pressing a key and delete a word

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-/* let elemeto = document.querySelector('table').addEventListener('change', (item) => {
-    console.log(item.path[0].value)
-    // console.log(item.path[0].attributes);
-}); */
-
-let countWords = 1;
+let countWords = 0;
+let selectedRow = 1;
 
 window.addEventListener("keydown", (event) => {
     console.log(event.key);
-    if(countWords <= 5 && event.key != 'Backspace' ){
+    if(countWords < 5 && event.keyCode >= 65 && event.keyCode <= 90){
+        countWords += 1;
         let element = document.getElementById(`word${countWords}`);
         element.append(event.key.toUpperCase());
-        countWords < 5 ? countWords += 1 : countWords = 5;
     }
     if(event.key == 'Backspace' && countWords > 0){
         let element = document.getElementById(`word${countWords}`);
-        console.log(element.innerHTML)
         element.innerHTML = '';
-        countWords > 1 ? countWords -= 1 : countWords = 1;
-        /* countWords -= 1; */
+        countWords -= 1;
     }
-    console.log(countWords);
+    if(event.key == 'Enter'){
+        countWords = 0;
+        let nextRow = selectedRow + 1;
+        for(let i = 0; i < 5; i++){
+            let currentDiv = document.getElementById(`row${selectedRow}`).children[i];
+            currentDiv.removeAttribute('id');
+            let nextDiv = document.getElementById(`row${nextRow}`).children[i];
+            nextDiv.setAttribute('id', `word${i+1}`);
+        }
+        selectedRow += 1;
+    }
 });
+
