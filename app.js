dc11281464daf881f9dfab86817547d60f8cbc7e
DanielAros <danielaros200@gmail.com>
2022-06-06 19:54:40 -0500

    Added function to change the color of the container to show if it is close to hitting the word

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const arrayWords = ['QUESO'];
+const arrayWords = ['QUESO', 'KOALA', 'PLUMA', 'POLLO', 'ACTOS', 'APILO', 'BROMA', 'CIEGO', 'CIELO', 'CIRCO', 'CARAS'];
 
 const indexRandomWord = Math.floor(Math.random() * arrayWords.length);
-console.log(arrayWords[indexRandomWord]);
+const chooseWord = arrayWords[indexRandomWord];
+console.log(chooseWord);
 
 let countLetters = 0;
 let selectedRow = 1;
 
 window.addEventListener("keydown", (event) => {
     /* console.log(event.key); */
-    if(countLetters < 5 && event.keyCode >= 65 && event.keyCode <= 90){
+    if(countLetters < 5 && event.keyCode >= 65 && event.keyCode <= 90 &&  selectedRow < 7){
         countLetters += 1;
         let element = document.getElementById(`word${countLetters}`);
         element.append(event.key.toUpperCase());
@@ -22,8 +23,8 @@ window.addEventListener("keydown", (event) => {
 
     if(event.key == 'Enter' && selectedRow < 6){
         const [counter, word] = isWord(selectedRow);
-        console.log(counter, word);
-        if(counter){
+        /* console.log(counter, word); */
+        if(counter && arrayWords.indexOf(word) != -1){
             countLetters = 0;
             let nextRow = selectedRow + 1;
             for(let i = 0; i < 5; i++){
@@ -32,16 +33,22 @@ window.addEventListener("keydown", (event) => {
                 let nextDiv = document.getElementById(`row${nextRow}`).children[i];
                 nextDiv.setAttribute('id', `word${i+1}`);
             }
+            isWordToSearch(word);
             selectedRow += 1;
-            console.log(selectedRow);
+            /* console.log(selectedRow); */
+        }else if(arrayWords.indexOf(word) == -1 && counter){
+            alert('La palabra no esta en la lista');
         }else{
             alert('No hay suficientes letras');
         }
     }else if(event.key == 'Enter' && selectedRow == 6){
         const [counter, word] = isWord(selectedRow);
-        if(counter){
+        if(counter && arrayWords.indexOf(word) != -1){
+            isWordToSearch(word);
             selectedRow++;
             console.log('game over');
+        }else if(arrayWords.indexOf(word) != -1 && counter){
+            alert('La palabra no esta en la lista');
         }else{
             alert('No hay suficientes letras');
         }
@@ -62,8 +69,29 @@ const isWord = (selectedRow) => {
     return [counter == 5, word];
 }
 
-const isWordToSearch = () => {
-    console.log(arrayWords)
+
+//This function changes the color of the container to show whether it is close to hitting the word.
+const isWordToSearch = (word) => {
+    //console.log(word);
+    // console.log(arrayWords[indexRandomWord] == word);
+    if(chooseWord == word){
+        for(let i = 0; i < 5; i++){
+            let div = document.getElementById(`row${selectedRow}`).children[i];
+            div.classList.add('green');
+        }
+        selectedRow = 7;
+    }else{
+        for(let i = 0; i < 5; i++){
+            let div = document.getElementById(`row${selectedRow}`).children[i];
+            if(chooseWord[i] == word[i]){
+                div.classList.add('green');
+            }else{
+                if(chooseWord.indexOf(word[i]) != -1){
+                    div.classList.add('yellow');
+                }else{
+                    div.classList.add('gray');
+                }
+            }
+        }
+    }
 }
-
-
